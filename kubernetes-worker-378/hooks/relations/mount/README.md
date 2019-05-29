f1d279b210570efcab14beed5581d3b8612fec03
MaaS LLC Server <ubuntu@LLCServer100.com>
2019-05-29 16:24:39 -0400

    RC3

--- kubernetes-worker-378/hooks/relations/mount/README.md
+++
@@ -1,2 +0,0 @@
-# interface-mount
-Interface layer for connecting to mounts to a charm such as NFS
--- kubernetes-worker-378/lib/debug_script.py
+++
@@ -1,8 +0,0 @@
-import os
-
-dir = os.environ["DEBUG_SCRIPT_DIR"]
-
-
-def open_file(path, *args, **kwargs):
-    """ Open a file within the debug script dir """
-    return open(os.path.join(dir, path), *args, **kwargs)
