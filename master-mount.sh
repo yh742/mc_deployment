ba778e92b261a77bf4698e27d7624310816ed12e
MaaS LLC Server <ubuntu@LLCServer100.com>
2019-06-04 15:26:55 -0400

    clean up mount script

--- master-mount.sh
+++ master-mount.sh
@@ -12,15 +12,21 @@ juju ssh $mon "sudo apt-get install -y ceph-deploy"
 # x in master node number
 for x in $masters
 do
-	juju ssh $x "yes y | ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa"
-	juju ssh $x "sudo mkdir /etc/ceph; sudo chmod -R 777 /etc/ceph"
-	juju ssh $mon "cat ~/.ssh/id_rsa.pub" | juju ssh $x "cat >> ~/.ssh/authorized_keys"
-	juju ssh $x "sudo apt-get install -y ceph-fuse"
+juju ssh $x <<EOF 
+yes y | ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
+sudo mkdir /etc/ceph
+sudo chmod -R 777 /etc/ceph
+sudo apt-get install -y ceph-fuse
+EOF
+juju ssh $mon "cat ~/.ssh/id_rsa.pub" | juju ssh $x "cat >> ~/.ssh/authorized_keys"
 done
 
 for x in $ips
 do 
-	juju ssh $mon "cd /etc/ceph;ceph-deploy install $x"
-	juju ssh $mon "cd /etc/ceph;ceph-deploy --overwrite-conf admin $x"
+juju ssh $mon <<EOF
+cd /etc/ceph
+ceph-deploy install $x
+ceph-deploy --overwrite-conf admin $x
+EOF
 done
 
